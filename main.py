47830249a4525c4e70ff736eee41040ccd85311a
Said <saidumarkhonalimkhanov@gmail.com>
2024-03-26 02:41:54 +0500

    RW Support added [alpha]

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import os
 import argparse
 from enum import Enum
 
-'''
-    TODO:
-    -- ADD BUFFER MODE SUPPORT (-b) argument where contents will be recorded to pre-allocated buf and print
-    -- IMPLEMENT CONFIG FILE SUPPORT where we can specify static arguments, color scheme support and etc
-    -- Read Write check
-    -- Date Time check
-    -- Detailed atributes and separate modular structure for project
-'''
-
 VERSION = "v0.1"
 DESCRIPTION_PROMPT = '''
 pyzax - minimalistic replacement for ls written in Python
@@ -33,26 +24,84 @@ class TerminalColor(Enum):
 def color_text(text, color):
     return f"{color.value}{text}{TerminalColor.RESET.value}"
 
-def list_files_and_folders(directory="."):
+def list_files_and_folders(directory=".", show_hidden=False):
     contents = os.listdir(directory)
     total_items = 0
+    total_hidden_items = 0
     print(color_text(f'Contents of {directory}:', TerminalColor.BOLD))
     print(color_text('-------------', TerminalColor.GREEN))
     for item in contents:
-        print(color_text(item, TerminalColor.YELLOW))
+        if not show_hidden and item.startswith('.'):
+            total_hidden_items += 1
+            continue
+        if os.path.isdir(os.path.join(directory, item)):
+            print(color_text(item, TerminalColor.YELLOW))  # Folders are printed in yellow
+        else:
+            print(color_text(item, TerminalColor.WHITE))  # Files are printed in white
         total_items += 1
-    print(color_text(f'Total {total_items} items', TerminalColor.BOLD))
+    if not show_hidden:
+        print(color_text(f'Total {total_items} items', TerminalColor.BOLD))
+    else:
+        print(color_text(f'Total {total_items} items. Hidden files - {total_hidden_items}.', TerminalColor.BOLD))
 
-def list_folders(directory="."):
+def check_rights(directory=".", show_hidden=False):
+    contents = os.listdir(directory)
+    total_items = 0
+    total_hidden_items = 0
+    max_name_length = max(len(item) for item in contents)  # Max length for proper alignment
+    print(color_text(f'Contents of {directory}:', TerminalColor.BOLD))
+    print(color_text('-------------', TerminalColor.GREEN))
+    for item in contents:
+        if not show_hidden and item.startswith('.'):
+            total_hidden_items += 1
+            continue
+        
+        if os.path.isdir(os.path.join(directory, item)):
+            item_color = TerminalColor.YELLOW  # Folders are displayed in yellow
+        else:
+            item_color = TerminalColor.WHITE  # Files are displayed in white
+        
+        # Constructing the file access rights string
+        rights_string = ""
+        if os.access(os.path.join(directory, item), os.R_OK):
+            rights_string += 'r'
+        else:
+            rights_string += '-'
+        if os.access(os.path.join(directory, item), os.W_OK):
+            rights_string += 'w'
+        else:
+            rights_string += '-'
+        if os.access(os.path.join(directory, item), os.X_OK):
+            rights_string += 'x'
+        else:
+            rights_string += '-'
+
+        # Printing the item with its rights
+        print(color_text(f'{rights_string.ljust(10)} {item.rjust(max_name_length)}', item_color))
+        total_items += 1
+    if not show_hidden:
+        print(color_text(f'Total {total_items} items', TerminalColor.BOLD))
+    else:
+        print(color_text(f'Total {total_items} items.', TerminalColor.BOLD))
+
+def list_folders(directory=".", show_hidden=False):
     contents = os.listdir(directory)
     total_folders = 0
+    total_hidden_folders = 0
     print(color_text(f'Folders in {directory}:', TerminalColor.BOLD))
     print(color_text('-------------', TerminalColor.GREEN))
     for item in contents:
+        if not show_hidden and item.startswith('.'):
+            total_hidden_folders += 1
+            continue
+        
         if os.path.isdir(os.path.join(directory, item)):
-            print(color_text(item, TerminalColor.YELLOW))
+            print(color_text(item, TerminalColor.YELLOW))  # Folders are printed in yellow
             total_folders += 1
-    print(color_text(f'Total {total_folders} folders', TerminalColor.BOLD))
+    if not show_hidden:
+        print(color_text(f'Total {total_folders} folders', TerminalColor.BOLD))
+    else:
+        print(color_text(f'Total {total_folders} folders. Hidden folders - {total_hidden_folders}.', TerminalColor.BOLD))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="List files in a directory")
@@ -60,11 +109,15 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--version", action="version", version=f"pyzax {VERSION}", help="Show program version")
     parser.add_argument("-d", "--description", action="store_true", help="Print program description")
     parser.add_argument("-f", "--folders", action="store_true", help="Print folders only")
+    parser.add_argument("-r", "--rights", action="store_true", help="Print file access rights")
+    parser.add_argument("-a", "--all", action="store_true", help="Show hidden elements")
     args = parser.parse_args()
 
     if args.description:
         print(DESCRIPTION_PROMPT)
     elif args.folders:
-        list_folders(directory=args.directory)
+        list_folders(directory=args.directory, show_hidden=args.all)
+    elif args.rights:
+        check_rights(directory=args.directory, show_hidden=args.all)
     else:
-        list_files_and_folders(directory=args.directory)
+        list_files_and_folders(directory=args.directory, show_hidden=args.all)
